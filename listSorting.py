2cc70f143c65af01ee56d1f6ad5b39a2d2967cb8
P. PRANEETH REDDY <praneethr1000@gmail.com>
2019-03-24 23:18:40 +0530

    Create listSorting.py

---
+++ listSorting.py
@@ -0,0 +1,12 @@
+N=int(input())
+a=[]
+for i in range(N):
+    b=list(map(int,input().split()))
+    for x in b:
+        a.append(x)
+a.sort()
+for y in range(len(a)):
+    if y!=len(a):
+        print(a[y],end=' ')
+    else:
+        print(a[y],end='')
