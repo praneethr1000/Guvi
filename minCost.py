4757632846ccc751543402030ac8d59d4aab0e6a
P. PRANEETH REDDY <praneethr1000@gmail.com>
2019-03-26 19:51:32 +0530

    Update minCost.py

--- minCost.py
+++ minCost.py
@@ -2,10 +2,22 @@ X,Y=input().split()
 X=list(X)
 Y=list(Y)
 c=0
-for i in range(len(Y)):
-    if Y[i] not in X:
-        c+=1
-    else:
-        if Y[i]!=X[i]:
+if len(Y)>len(X):
+    for i in range(len(Y)):
+        if Y[i] not in X:
             c+=1
+        else:
+            if Y[i]!=X[i]:
+                c+=1
+else:
+    for i in range(len(X)):
+        if X[i] not in Y:
+            c+=1
+        else:
+            if i<len(Y):
+                if X[i]!=Y[i]:
+                    c+=1
+            else:
+                c+=1
+
 print(c)
